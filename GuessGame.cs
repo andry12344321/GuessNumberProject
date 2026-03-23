9bcce36f500d7af0e12616f1d18f87cc2d463bb5
Lapika <8472121@stud.nau.edu.ua>
2026-03-23 15:16:51 +0200

    initial commit

---
+++ GuessGame.cs
@@ -0,0 +1,22 @@
+﻿public class GuessGame
+{
+    private int secretNumber;
+
+    public GuessGame(int min = 1, int max = 100)
+    {
+        Random random = new Random();
+        secretNumber = random.Next(min, max + 1);
+    }
+
+    public GuessGame(int fixedNumber)
+    {
+        secretNumber = fixedNumber;
+    }
+
+    public string CheckGuess(int guess)
+    {
+        if (guess < secretNumber) return "Больше";
+        if (guess > secretNumber) return "Меньше";
+        return "Угадал";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GuessNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            GuessGame game = new GuessGame();
+            int attempts = 0;
+            int guess = 0;
+
+            Console.WriteLine("Я загадал число от 1 до 100. Попробуй угадать!");
+
+            while (true)
+            {
+                Console.Write("Введите число: ");
+
+                if (!int.TryParse(Console.ReadLine(), out guess))
+                {
+                    Console.WriteLine("Введите корректное число!");
+                    continue;
+                }
+
+                attempts++;
+                string result = game.CheckGuess(guess);
+
+                if (result == "Угадал")
+                {
+                    Console.WriteLine($"Поздравляю! Ты угадал за {attempts} попыток.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(result);
+                }
+            }
+
+            Console.WriteLine("Нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
